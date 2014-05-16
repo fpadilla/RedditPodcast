3526fb28a969ef087731b8145924ce8d2cd8beca
fpadilla <fpadillao@gmail.com>
2014-05-16 15:51:56 -0400

    Disable default root route (spider problems)

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ RedditPodcast::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
- root 'post#list'
+ # root 'post#list'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
